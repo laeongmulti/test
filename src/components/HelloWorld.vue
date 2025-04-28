960552209597166d0283036f8df82dcdc8f14ca0
laeongmulti <laeongmulticampus@gmail.com>
2025-04-28 16:20:31 +0900

    123

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -11,7 +11,7 @@ defineProps({
   <div class="greetings">
     <h1 class="green">{{ msg }}</h1>
     <h3>
-      You’ve successfully created a project with1
+      You’ve successfully created a project withasdf
       <a href="https://vite.dev/" target="_blank" rel="noopener">Vite</a>+
       <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>.
       <a href="adsf" class="asdf asdf adf sdfds d" data="adf" id="asdf" key="asdf">adf</a>
